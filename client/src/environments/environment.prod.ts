5e4c08859645c8e65fc51afcadc3dd25643a83b5
andregomars <andregomars@gmail.com>
2018-09-16 23:42:14 -0700

    add firebase token

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -7,17 +7,17 @@ export const environment = {
   production: true,
   agm: { apiKey: 'AIzaSyCjplxp6Vku_R6LtWskqd0K7tSb0wLISPY' },
   firebase: {
-    apiKey: '',
-    authDomain: 'xxx.firebaseapp.com',
-    databaseURL: 'https://xxx.firebaseio.com',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyCjplxp6Vku_R6LtWskqd0K7tSb0wLISPY',
+    authDomain: 'fancycandemo.firebaseapp.com',
+    databaseURL: 'https://fancycandemo.firebaseio.com',
+    projectId: 'fancycandemo',
+    storageBucket: 'fancycandemo.appspot.com',
+    messagingSenderId: '459557828534'
   },
   apiRootLocal: 'assets/data',
   reportFallbackMonths: 3,
   delayEmulatorTimer: 2000,
   loadMap: true,
-  dataSource: DataSourceType.Local
+  dataSource: DataSourceType.Firebase
 };
 
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -7,12 +7,12 @@ export const environment = {
   production: false,
   agm: { apiKey: 'AIzaSyCjplxp6Vku_R6LtWskqd0K7tSb0wLISPY' },
   firebase: {
-    apiKey: '',
-    authDomain: 'xxx.firebaseapp.com',
-    databaseURL: 'https://xxx.firebaseio.com',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyCjplxp6Vku_R6LtWskqd0K7tSb0wLISPY',
+    authDomain: 'fancycandemo.firebaseapp.com',
+    databaseURL: 'https://fancycandemo.firebaseio.com',
+    projectId: 'fancycandemo',
+    storageBucket: 'fancycandemo.appspot.com',
+    messagingSenderId: '459557828534'
   },
   apiRootLocal: 'assets/data',
   reportFallbackMonths: 3,
