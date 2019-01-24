ed6cadc2c4a1aa6924604fe503a414feee089257
andre <andregomars@gmail.com>
2019-01-24 09:45:35 -0800

    remove dist from server project

--- server/dist/server/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var application_1 = require("./src/app/application");
-var app = new application_1.Application();
-app.start();
-// (async () => {
-//     app.start();
-// })();
