2f85c8553a8df2c508e904068ab358693f9c16ba
Andre <andregomars@gmail.com>
2019-01-28 16:50:23 -0800

    update app.js location in pm2 config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'fcrx',
-    script: 'dist/server/app.js',
+    script: 'dist/app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
