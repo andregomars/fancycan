b54e20320c741bdd5c048cc36851f4f141dcccc1
Andre <andregomars@gmail.com>
2019-03-14 19:00:02 -0700

    implement pausible timer in vehicle page

--- client/src/app/views/vehicle/vehicle.component.ts
+++ client/src/app/views/vehicle/vehicle.component.ts
@@ -1,57 +1,34 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { getStyle, hexToRgba } from '@coreui/coreui-pro/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { Observable, timer, of, BehaviorSubject, NEVER } from 'rxjs';
+import { share, map, tap, switchMap, shareReplay } from 'rxjs/operators';
+import { Select } from '@ngxs/store';
 
-import { Observable, timer, of } from 'rxjs';
 import { DataService, UtilityService } from '../../services';
-import { share, map, tap, switchMap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 import { MapStyle } from './../shared/map-style';
-import { Select } from '@ngxs/store';
 import { ViewProfileState, SpnProfileState } from '../../states';
 import { PlayChartComponent } from '../../components/play-chart/play-chart.component';
-// import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-vehicle',
   templateUrl: './vehicle.component.html',
   styleUrls: ['./vehicle.component.scss'],
-  // animations: [
-  //   // the fade-in/fade-out animation.
-  //   trigger('simpleFadeAnimation', [
-
-  //     // the "in" style determines the "resting" state of the element when it is visible.
-  //     state('in', style({ opacity: 1 })),
-
-  //     // fade in when created. this could also be written as transition('void => *')
-  //     transition(':enter', [
-  //       style({ opacity: 0 }),
-  //       animate(600)
-  //     ]),
-
-  //     // fade out when destroyed. this could also be written as transition('void => *')
-  //     transition(':leave',
-  //       animate(600, style({ opacity: 0 })))
-  //   ])
-  // ]
 })
 export class VehicleComponent implements OnInit, OnDestroy {
   @Select(ViewProfileState.vcode) vcode$: Observable<string>;
   @Select(SpnProfileState.spns) spns$: Observable<any[]>;
   @ViewChild('playChart') playChart: PlayChartComponent;
+  pauser = new BehaviorSubject<boolean>(false);
 
-  isAutoSync = true;
+  intSecPlayChart = 1;
   intSecState = 7;
-  itv: any;
   len = 30;
-  intSec = 1;
   temperatureMax = 300;
   temperatureMin = -300;
-  queue: any[];
   alerts$: Observable<any>;
-  // decodes$: Observable<any>;
   vehicleStateSpnList$: Observable<any>;
-  vehicles$: Observable<any>;
   vehicle$: Observable<any>;
   playChartData$: Observable<any>;
   playChartLabels: string[];
@@ -65,18 +42,12 @@ export class VehicleComponent implements OnInit, OnDestroy {
   map_lat = 34.056539;
   map_lgt = -118.237485;
 
-  // gauge config
-  gaugeForegroundColor = '#17a2b8';
-  gaugeType = 'semi';
-  gaugeThick = 15;
+  // // gauge config
+  // gaugeForegroundColor = '#17a2b8';
+  // gaugeType = 'semi';
+  // gaugeThick = 15;
 
-  lastUpdated = '2018-08-28 23:32:55';
-  // currentTime = new Date();
   currentTime$: Observable<Date>;
-  engineRunning = 45;
-  engineIdle = 60;
-  odometer = 27026.8;
-  vin = 'V12W132456107';
   imgBus = 'assets/img/vehicle/bus.png';
   imgEngineCheck = 'assets/img/vehicle/check_engine.png';
   imgTransmissionCheck = 'assets/img/vehicle/check_transmission.png';
@@ -135,79 +106,68 @@ export class VehicleComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.loadTimeLabel();
-    this.loadVehicleState();
-    this.loadVehicleStatePeriodically();
+    this.loadVehicle();
+    this.loadSpns();
+    this.loadAlerts();
     this.loadPlayChartData();
-    // this.playChart.pause();
   }
 
   ngOnDestroy() {
-    this.clearAutoSync();
-  }
-
-  switchSyncStatus() {
-    this.isAutoSync = !this.isAutoSync;
-    if (this.isAutoSync) {
-      this.loadVehicleStatePeriodically();
-    } else {
-      this.clearAutoSync();
+    if (this.pauser) {
+      this.pauser.unsubscribe();
     }
   }
 
-  clearAutoSync() {
-    if (this.itv) {
-      clearInterval(this.itv);
-    }
+  toggleSyncStatus() {
+    this.pauser.next(!this.pauser.value);
+  }
+
+  private loadVehicle() {
+    const vehicleOnTimer$ = timer(0, this.intSecState * 1000).pipe(
+      switchMap(() =>
+        this.vcode$.pipe(
+          switchMap(vcode => this.dataService.getVehicleState(vcode)),
+          map(vehicles => this.utilityService.attachGeoLocationAndMapLabel(vehicles)[0]),
+      )),
+      share()
+    );
+
+    this.vehicle$ = this.pauser.pipe(
+      switchMap(paused => paused ? NEVER : vehicleOnTimer$)
+    );
+
+  }
+
+  private loadSpns() {
+    this.vehicleStateSpnList$ = this.vehicle$.pipe(
+        switchMap(state => this.spns$.pipe(
+          map(spns => this.utilityService.buildVehicleStateSpnList(state, spns))
+        )),
+    );
+  }
+
+  private loadAlerts() {
+    const alertOnTimer$ = timer(0, this.intSecState * 1000).pipe(
+      switchMap(() =>
+        this.vcode$.pipe(
+          switchMap(vcode => this.dataService.getVehicleMalfuncStatesByLatestN(vcode, 5)),
+      ))
+    );
+
+    this.alerts$ = this.pauser.pipe(
+      switchMap(paused => paused ? NEVER : alertOnTimer$)
+    );
   }
 
   private loadTimeLabel() {
-    // this.currentTime$ = new Observable<Date>((observer: Observer<Date>) => {
-    //   setInterval(() => observer.next(new Date()), 1000);
-    // });
     this.currentTime$ = timer(0, 1000).pipe(
       map(() => new Date())
     );
   }
 
-  private loadVehicleStatePeriodically() {
-    this.itv = setInterval(() => {
-      this.loadVehicleState();
-    }, this.intSecState * 1000);
-  }
-
-  private loadVehicleState() {
-    // this.vehicle$ = this.vcode$.pipe(
-    //   switchMap(vcode => this.dataService.getVehicleState(vcode)),
-    //   map(vehicles => this.utilityService.attachGeoLabels(vehicles[0])),
-    //   share()
-    // );
-
-    this.vehicle$ = this.vcode$.pipe(
-      switchMap(vcode => this.dataService.getVehicleState(vcode)),
-      map(vehicles => this.utilityService.attachGeoLocationAndMapLabel(vehicles)[0]),
-      share()
-    );
-
-    this.vehicleStateSpnList$ = this.vehicle$.pipe(
-      switchMap(state => this.spns$.pipe(
-        map(spns => this.utilityService.buildVehicleStateSpnList(state, spns))
-      ))
-    );
-
-    this.alerts$ = this.vcode$.pipe(
-      switchMap(vcode => this.dataService.getVehicleMalfuncStatesByLatestN(vcode, 5)),
-      // map((alerts: any[]) => alerts.slice(0, 5)),
-      share()
-    );
-
-    // this.decodes$ = this.dataService.getDecodes().pipe(
-    //   share()
-    // );
-  }
-
   private loadPlayChartData() {
     this.playChartLabels = Array.from(new Array(this.len), (v, i) =>
-      `${((this.len - i - 1) * this.intSec)}s`
+      `${((this.len - i - 1) * this.intSecPlayChart)}s`
     );
     // this.playChartData$ = this.utilityService.getCurrentData();
     // this.playChartData$ = timer(0, this.intSec * 1000).pipe(map(n => n * 100));
